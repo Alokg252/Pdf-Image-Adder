c22e27c61761e25f13de7f4e1b220e9060a10867
Alok Gupta <73645127+Alokg252@users.noreply.github.com>
2025-03-16 08:26:47 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: 'Pdf-Image-Adder',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+  });
